b557ad6a0a7fdf7ea0f46ed595fe3d23dab7b46a
Neil Smith <neil.git@njae.me.uk>
2019-05-16 16:13:19 +0100

    Formatted ack message

--- remote-microbit/remote.py
+++ remote-microbit/remote.py
@@ -11,16 +11,16 @@ while True:
     rcvd = radio.receive()
     if rcvd:
         # received = {k.strip(): v.strip() for [k, v] in [term.strip().split(':') for term in rcvd.split(';')]}
-        microbit.display.scroll(rcvd.strip(), loop=True, wait=False)
+        # microbit.display.scroll(rcvd.strip(), loop=True, wait=False)
         terms = rcvd.strip().split(';')
+        # microbit.display.scroll('*'.join(terms), loop=True, wait=False)
+
         pairs = [term.strip().split(':') for term in terms]
         received = {k.strip() : v.strip() for [k, v] in pairs}
-        microbit.display.scroll('*'.join(received.values()), loop=True, wait=False)
+        # microbit.display.scroll('*'.join(received.values()), loop=True, wait=False)
 
         if int(received.get('id', 0)) == ID:
             if 'message' in received:
                 microbit.display.scroll(received['message'], loop=True, wait=False)
             microbit.sleep(100)
-            radio.send('ack {}'.format(ID))
-        rcvd = None
-        received = None
+            radio.send('ack:{}'.format(ID))
