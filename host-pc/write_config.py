b713c2931db83784e199be4e8f70e4ddd9c77ea7
Neil Smith <neil.git@njae.me.uk>
2019-05-19 18:31:38 +0100

    Fixed a couple of bugs

--- host-pc/write_config.py
+++ host-pc/write_config.py
@@ -3,7 +3,7 @@ config = configparser.ConfigParser()
 config['DEFAULT'] = {
     'username': 'Molly',
     'token': 'e25000699092945b68e8a377ccc4635549e5851e',
-    'hostname': 'localhost:8000/library/api/v1/',
+    'hostname': 'https://localhost:8000/library/api/v1/',
     }
 
 with open('microbit.ini', 'w') as configfile:
